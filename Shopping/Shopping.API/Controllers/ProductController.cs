9f5dfa96df9476b35739480ac3ed43099807b6ff
Chuks Enedeh <cenedeh@anchor-xtem.com>
2022-08-21 12:33:23 +0100

    Add partial implementation of product endpoint

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
